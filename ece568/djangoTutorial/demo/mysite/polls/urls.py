b03a471f466e9d3a9dc5aaf3ead128afca355cc2
zw154 <zuhao.wu@outlook.com>
2019-01-30 02:42:21 -0500

    after generic views

--- ece568/djangoTutorial/demo/mysite/polls/urls.py
+++ ece568/djangoTutorial/demo/mysite/polls/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+'''
+#before using generic views
 app_name = 'polls' # used for namespacing 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -10,5 +12,16 @@ urlpatterns = [
     # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+'''
+# using generic views, change index, detail, results three view functions
+# "pk"--primary key
+# some function change
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
