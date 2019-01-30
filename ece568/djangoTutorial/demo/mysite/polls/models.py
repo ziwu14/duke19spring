ff7066998b4a07cd83f87346e4670128d8b21e68
zw154 <zuhao.wu@outlook.com>
2019-01-30 15:49:51 -0500

    customize fields, change list, add filter and search bars

--- ece568/djangoTutorial/demo/mysite/polls/models.py
+++ ece568/djangoTutorial/demo/mysite/polls/models.py
@@ -27,6 +27,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
     
 class Choice(models.Model):
     # Foreignkey--to associate the Choice to a specific Question class i.e. define a relationship
