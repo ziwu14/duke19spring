76e27e304af58aba85a16d385653639b60a5378c
zw154 <zuhao.wu@outlook.com>
2019-02-01 22:03:28 -0500

    before trying customize user model

---
+++ ece568/hw1/mysite/temp/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+
+from . import views
+
+'''
+#before using generic views
+app_name = 'polls' # used for namespacing 
+urlpatterns = [
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+'''
+# using generic views, change index, detail, results three view functions
+# "pk"--primary key
+# some function change
+app_name = 'temp'
+urlpatterns = [
+    path('aaa', views.aaa, name='aaa'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
