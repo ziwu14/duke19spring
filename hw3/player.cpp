600bf5521c455fa55e925bb13a14891e7ead054d
zw154 <zw154@duke.edu>
2019-02-15 00:45:23 -0500

    gain client's address on server, gain client's address on client

--- hw3/player.cpp
+++ hw3/player.cpp
@@ -10,6 +10,18 @@
 
 using namespace std;
 
+void obtainMyAddr(int socket_fd, char *ipPtr, char*portPtr){
+    
+    struct sockaddr_in peerAddr;
+    //bzero(&myAddress, sizeof(myAddress));
+    socklen_t len = sizeof(peerAddr);
+    getsockname(socket_fd,(struct sockaddr *) &peerAddr,&len);
+    inet_ntop(AF_INET, &peerAddr.sin_addr, ipPtr, 16*sizeof(ipPtr));
+    int temp = ntohs(peerAddr.sin_port);
+    sprintf(portPtr,"%d",temp);
+}
+
+
 int main(int argc, char *argv[])
 {
   
@@ -18,7 +30,7 @@ int main(int argc, char *argv[])
     exit(EXIT_FAILURE);
   }
 
-  //1.create socket
+  //1.create socket -- socket -> local
   int server_fd;
   struct sockaddr_in server_address;
 
@@ -27,28 +39,27 @@ int main(int argc, char *argv[])
     perror("socket() fails");
     exit(EXIT_FAILURE);
   }
+
+  //2.connect to server via connect function which could return information of server socket address
   memset(&server_address, 0, sizeof(server_address));
   server_address.sin_family = AF_INET;
   server_address.sin_addr.s_addr = inet_addr(argv[1]);
   server_address.sin_port = htons(4444);
 
-  //2.connect
   if ( connect(server_fd, (struct sockaddr*)&server_address, sizeof(server_address)) == -1  ){
     perror("connect() fails");
     exit(EXIT_FAILURE);
   }
-
-  while(1){
-    string temp;
-    cout<<"input sth"<<endl;
-    cin >> temp;
+    
+    char myIP[16]={0};
+    char myPort[16]={0};
+    obtainMyAddr(server_fd, myIP,myPort);
+    cout<<"my ip: "<<myIP<<" my port: "<<myPort<<endl;
+    
     char buffer[512];
     strcpy(buffer,temp.c_str());
     send(server_fd, buffer,strlen(buffer),0);
-    if(strcmp(buffer,"q") == 0){
-      break;
-    }
-  }
+    
   
   close(server_fd);
 
--- hw3/ringmaster.cpp
+++ hw3/ringmaster.cpp
@@ -8,14 +8,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <vector>
+#include <utility>
 
 using namespace std;
 
-void wait_allClients(fd_set &reads, int &fd_max, vector<int> &client_fdList,struct timeval timeout,int server_fd){
+void obtainPeerAddr(int socket_fd, char *ipPtr, char*portPtr){
+    
+    struct sockaddr_in peerAddr;
+    //bzero(&myAddress, sizeof(myAddress));
+    socklen_t len = sizeof(peerAddr);
+    getpeername(socket_fd,(struct sockaddr *) &peerAddr,&len);
+    inet_ntop(AF_INET, &peerAddr.sin_addr, ipPtr, 16*sizeof(ipPtr));
+    int temp = ntohs(peerAddr.sin_port);
+    sprintf(portPtr,"%d",temp);
+}
+
+void Server_to_Clients(fd_set &reads, int &fd_max, vector<int> &client_fdList,vector<pair<char*,char*> > &client_addrList,struct timeval timeout,int listen_fd){
 
   FD_ZERO(&reads);//00000000000000000
-  FD_SET(server_fd, &reads);//00000001000000000
-  fd_max = server_fd;//???????
+  FD_SET(listen_fd, &reads);//00000001000000000
+  fd_max = listen_fd;//???????
   
   int client_fd;
   struct sockaddr_in client_address;
@@ -37,18 +49,29 @@ void wait_allClients(fd_set &reads, int &fd_max, vector<int> &client_fdList,stru
     for(int fd = 0; fd < fd_max + 1; fd++){
       if(FD_ISSET(fd, &temp)){//check which file descriptor changes
 	
-	if(fd == server_fd){//connection request: add the client_fd to the fd_set
+	if(fd == listen_fd){//connection request: add the client_fd to the fd_set
 	  
 	  address_size = sizeof(client_fd);
-	  client_fd = accept(server_fd, (struct sockaddr*)&client_address,&address_size);
+	  client_fd = accept(listen_fd, (struct sockaddr*)&client_address,&address_size);
 
+	  
+	  char clientIP[16]={0};
+	  char clientPort[16]={0};
+	  obtainPeerAddr(client_fd,clientIP,clientPort);
+	  cout<< "client ip: "<<clientIP<<"client port: "<<clientPort<<endl;
+	  
 	  FD_SET(client_fd, &reads);
 	  client_fdList.push_back(client_fd);
+	  client_addrList.push_back(pair<char*,char*>(clientIP,clientPort));
+	  //cout<<"client ip: "<<client_addrList[client_count].first<<endl;
+	  //cout<<"client port: "<<client_addrList[client_count].second<<endl;
 	  
 	  if(fd_max < client_fd){//update fd_max when new socket is involved
 	    fd_max = client_fd;
 	    cout<<"new client is connecting"<<endl;
 	  }
+
+	  client_count++;
 	  
 	} else{//receive message from client_fd i.e. client_fd is ready to be read
 
@@ -59,7 +82,7 @@ void wait_allClients(fd_set &reads, int &fd_max, vector<int> &client_fdList,stru
 	  cout<<fd<<endl;
 	  
 	  if(strcmp(buffer,"q") == 0){//exit signal
-	    client_count++;
+	    
 	    break;
 	  }
 	  
@@ -71,27 +94,39 @@ void wait_allClients(fd_set &reads, int &fd_max, vector<int> &client_fdList,stru
   }
 }
 
+void Clients_to_Clients(const vector<int> &client_fdList,struct timeval timeout){
+  for(size_t i = 0; i < client_fdList.size(); i++){
+    
+  }
+}
+
 int main(int argc, char *argv[])
 {
-  int server_fd;
+  int listen_fd;
   struct sockaddr_in server_address;
   
 
-  //1. socket setup
-  server_fd = socket(AF_INET, SOCK_STREAM, 0);//family, type, anything
+  //1. socket create -- defined by protocol, type
+  listen_fd = socket(AF_INET, SOCK_STREAM, 0);//family, type, anything
+  if(listen_fd < 0){
+    perror("socket() fails");
+    exit(EXIT_FAILURE);
+  }
+  
+  //2. bind() to an address -- defined by ip, port, protocol-> server_address
+  //otherwise, it will be automatically allocated a port, clients have no idea what it is
   memset(&server_address, 0, sizeof(server_address));//clear server_address
-  server_address.sin_family = AF_INET;
+  server_address.sin_family = AF_INET;//address family
   server_address.sin_addr.s_addr = htonl(INADDR_ANY);//ip address
   server_address.sin_port = htons(4444);//port
 
-  //2. bind() to an address
   socklen_t address_size = sizeof(server_address);
-  if(bind(server_fd, (struct sockaddr*)&server_address, address_size) == -1){//return socket address
+  if(bind(listen_fd, (struct sockaddr*)&server_address, address_size) == -1){//return socket address
     perror("bind() fails");
     exit(EXIT_FAILURE);
   }
   //3.listen
-  if(listen(server_fd,10) == -1){
+  if(listen(listen_fd,10) == -1){
     perror("listen() fails");
     exit(EXIT_FAILURE);
   }
@@ -100,20 +135,21 @@ int main(int argc, char *argv[])
   struct timeval timeout;
   timeout.tv_sec = 5;
   timeout.tv_usec = 5000;
-  vector<int> client_fdList;//record order of connection
+  vector<int> client_fdList;//record order of connection, to close them at the end
+  vector<pair<char*,char*> > client_addrList;
   fd_set reads;//fd_set including server and all clients
   int fd_max;//the largest fd number
 
-  wait_allClients(reads, fd_max, client_fdList,timeout,server_fd);
+  Server_to_Clients(reads, fd_max, client_fdList,client_addrList,timeout,listen_fd);
   
   //4.2.connect clients to their neighbors
-  
+  Clients_to_Clients(client_fdList,timeout);
   
   
 
   for(size_t i = 0; i < client_fdList.size(); i++){
     close(client_fdList[i]);
   }
-  close(server_fd);
+  close(listen_fd);
   return 0;
 }
