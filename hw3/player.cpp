9c67852a7c6ea2245ddefcd87348c19b3d37043f
zw154 <zw154@duke.edu>
2019-02-14 20:52:06 -0500

    select succeeds

--- hw3/player.cpp
+++ hw3/player.cpp
@@ -12,70 +12,45 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  int status;
-  int socket_fd;
-  struct addrinfo host_info;
-  struct addrinfo *host_info_list;
-  const char *hostname = argv[1];//IP address=host name e.g. 127.0.0.1
-  const char *port     = "4444";//port#
   
-  if (argc < 2) {
-      cout << "Syntax: client <hostname>\n" << endl;
-      return 1;
+  if(argc != 2){
+    perror("Usage: ./server [server ip]");
+    exit(EXIT_FAILURE);
   }
 
-  //1. get the host information by getaddrinfo function, result is stored in host_info_list
-  memset(&host_info, 0, sizeof(host_info));//clear host_info struct
-  host_info.ai_family   = AF_INET;//indicates that getaddrinfo() should return socket addresss for any address family(e.g. either IPv4 or IPv6) that can be used with node and service
-  host_info.ai_socktype = SOCK_STREAM;//one of the connection based socket
+  //1.create socket
+  int server_fd;
+  struct sockaddr_in server_address;
 
-  status = getaddrinfo(hostname, port, &host_info, &host_info_list);//the result is stored in host_info_list, host_info is used to get rid of warning
-  if (status != 0) {
-    cerr << "Error: cannot get address info for host" << endl;
-    cerr << "  (" << hostname << "," << port << ")" << endl;
-    return -1;
-  } //if
-  
-  //2.get the socket file descriptor -> socket is specific to combination of address familty + socket type + protocol
-  socket_fd = socket(host_info_list->ai_family, 
-		     host_info_list->ai_socktype, 
-		     host_info_list->ai_protocol);
-  if (socket_fd == -1) {
-    cerr << "Error: cannot create socket" << endl;
-    cerr << "  (" << hostname << "," << port << ")" << endl;
-    return -1;
-  } //if
-  
-  cout << "Connecting to " << hostname << " on port " << port << "..." << endl;
+  server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if(server_fd == -1){
+    perror("socket() fails");
+    exit(EXIT_FAILURE);
+  }
+  memset(&server_address, 0, sizeof(server_address));
+  server_address.sin_family = AF_INET;
+  server_address.sin_addr.s_addr = inet_addr(argv[1]);
+  server_address.sin_port = htons(4444);
 
-  //3.connect, return addr and addrlen
-  status = connect(socket_fd, host_info_list->ai_addr, host_info_list->ai_addrlen);
-  if (status == -1) {
-    cerr << "Error: cannot connect to socket" << endl;
-    cerr << "  (" << hostname << "," << port << ")" << endl;
-    return -1;
-  } //if
-  
-  char clientIP[16];
-  unsigned int clientPort;
-  struct sockaddr_in client_addr;
-  //bzero(&client_addr, sizeof(client_addr));
-  socklen_t len = sizeof(client_addr);
-  getsockname(socket_fd,(struct sockaddr *) &client_addr,&len);
-  inet_ntop(AF_INET, &client_addr.sin_addr, clientIP, sizeof(clientIP));
-  clientPort = ntohs(client_addr.sin_port);
-  printf("ip: %s  , port:  %u\n",clientIP,clientPort);
-  
+  //2.connect
+  if ( connect(server_fd, (struct sockaddr*)&server_address, sizeof(server_address)) == -1  ){
+    perror("connect() fails");
+    exit(EXIT_FAILURE);
+  }
 
-  //send(socket_fd, "hi there!", 9, 0);
-  //char buffer[512];
-  char buffer[512];
-  int end = recv(socket_fd, buffer, sizeof(buffer), 0);
-  buffer[end] = 0;
-  cout<< "client received: " << buffer<<endl;
+  while(1){
+    string temp;
+    cout<<"input sth"<<endl;
+    cin >> temp;
+    char buffer[512];
+    strcpy(buffer,temp.c_str());
+    send(server_fd, buffer,strlen(buffer),0);
+    if(strcmp(buffer,"q") == 0){
+      break;
+    }
+  }
   
-  freeaddrinfo(host_info_list);
-  close(socket_fd);
+  close(server_fd);
 
   return 0;
 }
--- hw3/ringmaster.cpp
+++ hw3/ringmaster.cpp
@@ -7,105 +7,100 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <vector>
 
 using namespace std;
 
-
-
-
 int main(int argc, char *argv[])
 {
-  int status;
-  int socket_fd;
-  struct addrinfo host_info;//to avoid warning
-  struct addrinfo *host_info_list;//result is stored here
-  const char *hostname = NULL;//IP will be set to loopback
-  const char *port     = "4444";
+  int server_fd;
+  int client_fd;
+  struct sockaddr_in server_address;
+  struct sockaddr_in client_address;
+  socklen_t address_size;
 
-  //1. get the host_info by getaddrinfo, which returns host_info_list
-  memset(&host_info, 0, sizeof(host_info));//clear the warning remover
 
-  host_info.ai_family   = AF_INET;
-  host_info.ai_socktype = SOCK_STREAM;
-  host_info.ai_flags    = AI_PASSIVE;//for wildcard IP address, which means "any" and can only be used for bind operations i.e. 0.0.0.0 
+  //1. socket setup
+  server_fd = socket(AF_INET, SOCK_STREAM, 0);//family, type, anything
+  memset(&server_address, 0, sizeof(server_address));//clear server_address
+  server_address.sin_family = AF_INET;
+  server_address.sin_addr.s_addr = htonl(INADDR_ANY);//ip address
+  server_address.sin_port = htons(4444);//port
 
-  status = getaddrinfo(hostname, port, &host_info, &host_info_list);
-  if (status != 0) {
-    cerr << "Error: cannot get address info for host" << endl;
-    cerr << "  (" << hostname << "," << port << ")" << endl;
-    return -1;
-  } //if
-  //2. create socket, which returns the socket file descriptor
-  socket_fd = socket(host_info_list->ai_family, 
-		     host_info_list->ai_socktype, 
-		     host_info_list->ai_protocol);
-  if (socket_fd == -1) {
-    cerr << "Error: cannot create socket" << endl;
-    cerr << "  (" << hostname << "," << port << ")" << endl;
-    return -1;
-  } //if
+  //2. bind() to an address
+  address_size = sizeof(server_address);
+  if(bind(server_fd, (struct sockaddr*)&server_address, address_size) == -1){//return socket address
+    perror("bind() fails");
+    exit(EXIT_FAILURE);
+  }
+  //3.listen
+  if(listen(server_fd,10) == -1){
+    perror("listen() fails");
+    exit(EXIT_FAILURE);
+  }
 
-  //3. set address via bind()
-  int yes = 1;
-  status = setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
-  status = bind(socket_fd, host_info_list->ai_addr, host_info_list->ai_addrlen);
-  if (status == -1) {
-    cerr << "Error: cannot bind socket" << endl;
-    cerr << "  (" << hostname << "," << port << ")" << endl;
-    return -1;
-  } //if
+  //4.select() & accept()
+  struct timeval timeout;
+  fd_set reads, temp;//select() function will only leave fd to change so we use a temp to pass into select
+  int fd_max;
+  vector<int> client_fdList;
+  
+  FD_ZERO(&reads);//00000000000000000
+  FD_SET(server_fd, &reads);//00000001000000000
+  fd_max = server_fd;//???????
+  timeout.tv_sec = 5;
+  timeout.tv_usec = 5000;
 
-  
-  const char * host_ip = inet_ntoa(((struct sockaddr_in*)host_info_list->ai_addr)->sin_addr);
-  unsigned int host_portINT = ntohs(((struct sockaddr_in*)host_info_list->ai_addr)->sin_port);
-  char host_port[16]={0};
-  sprintf(host_port,"%d",host_portINT);
-  printf("ip: %s  port: %s\n",host_ip,host_port);
+  int game_over = 0;
+  while(game_over != 1){
+    
+    temp = reads;
+    int status= select(fd_max + 1, &temp, 0, 0, &timeout);
+    if(status == -1){
+      perror("select() fails");
+    }
+    else if(status == 0){
+      //cout<< "timeout"<<endl;
+      continue;
+    }
+
+    for(int fd = 0; fd < fd_max + 1; fd++){
+      if(FD_ISSET(fd, &temp)){//check which file descriptor changes
+	
+	if(fd == server_fd){//connection request
+	  
+	  address_size = sizeof(client_fd);
+	  client_fd = accept(server_fd, (struct sockaddr*)&client_address,&address_size);
+
+	  FD_SET(client_fd, &reads);
+	  client_fdList.push_back(client_fd);
+	  if(fd_max < client_fd){//update fd_max when new socket is involved
+	    fd_max = client_fd;
+	    cout<<"new client is connecting"<<endl;
+	  }
+	  
+	} else{//receive message from client_fd i.e. client_fd is ready to be read
+
+	  cout<<"changed fd is "<<fd<<endl;
+	  char buffer[512]={0};
+	  recv(fd,buffer,sizeof(buffer),0);
+	  cout<<fd<<endl;
+	  if(strcmp(buffer,"q") == 0){
+	    game_over = 1;
+	    break;
+	  }
+	  
+	}
+
+      }
+    }
+    
+  }
   
 
-  //4, listen
-  status = listen(socket_fd, 100);
-  if (status == -1) {
-    cerr << "Error: cannot listen on socket" << endl; 
-    cerr << "  (" << hostname << "," << port << ")" << endl;
-    return -1;
-  } //if
-
-  //5.accept
-  cout << "Waiting for connection on port " << port << endl;
-  struct sockaddr_storage socket_addr;
-  socklen_t socket_addr_len = sizeof(socket_addr);
-  int client_connection_fd;
-  client_connection_fd = accept(socket_fd, (struct sockaddr *)&socket_addr, &socket_addr_len);
-  if (client_connection_fd == -1) {
-    cerr << "Error: cannot accept connection on socket" << endl;
-    return -1;
-  } //if
-
-  const char * client_ip = inet_ntoa( ((struct sockaddr_in*)&socket_addr)->sin_addr);
-  int client_portINT = ntohs( ((struct sockaddr_in*)&socket_addr)->sin_port );
-  char client_port[100]={0};
-  sprintf(client_port,"%d",client_portINT);
-  printf("ip: %s  port: %s\n",client_ip,client_port);
-  
-  
-  
-  //6.read/write
-  
-  
-  //char buffer[512];
-  //int end = recv(client_connection_fd, buffer, sizeof(buffer), 0);
-  //buffer[end] = 0;
-  //cout << "Server received: " << buffer << endl;
-  
-  send(client_connection_fd, client_ip,strlen(client_ip),0);
-  
-  
-  //send(socket_fd,client_ip,strlen(client_ip), 0);
- 
-  freeaddrinfo(host_info_list);
-  close(socket_fd);
-
+  for(size_t i = 0; i < client_fdList.size(); i++){
+    close(client_fdList[i]);
+  }
+  close(server_fd);
   return 0;
 }
